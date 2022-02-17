33799f12d5a50971451df1ddab76ea78c9c75469
bindi-v <bindi.vaghela@gmail.com>
2022-02-17 12:47:20 -0500

    Initial commit for deploy to Netlify

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Records from "./Records"
+
+function App() {
+  return (
+    <main className='container'>
+      <Records />
+    </main>
+  );
+}
+
+export default App;
